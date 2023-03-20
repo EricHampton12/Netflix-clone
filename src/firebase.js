e96a169dba0eeabcc1bca1fe0afeab028ef7b037
EricHampton12 <EricHampton2010@gmail.com>
2023-03-20 15:32:04 -0400

    added login and register functionality

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBkO_lCpGfUH-ut99zAL13Fg-NMzexBHW4",
   authDomain: "netflix-v2-212e9.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "689884671816",
   appId: "1:689884671816:web:21cd464a52c895e2c9cd7d",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
